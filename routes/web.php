d84c13a9ad6c2289f8ce854966fc50ea3c6582cc
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-11-06 14:36:16 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/fetch', 'HomeController@fetchMessage');
+Route::post('/send', 'HomeController@sendMessage');
